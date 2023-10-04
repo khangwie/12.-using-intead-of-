eadc2564d6211245589cf6e2c37301dfd857ae8e
Lenovo-User <Lenovo-User@DESKTOP-DGL558B>
2023-10-04 20:58:39 +0200

    Add project files.

---
+++ 12. using & intead of +/Form1.vb
@@ -0,0 +1,10 @@
+﻿Public Class Form1
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        Dim text1, text3 As String
+        Dim Text2 As Integer
+        text1 = "Visual"
+        Text2 = 22
+        text3 = text1 & Text2
+        MsgBox(text3)
+    End Sub
+End Class
